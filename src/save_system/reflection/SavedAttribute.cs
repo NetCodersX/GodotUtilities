9273590429da43fdcdd5df5c96a34def8896e142
Ahmed GD <104320450+AhmedGD1@users.noreply.github.com>
2026-05-12 13:16:03 +0300

    Delete src directory

--- src/save_system/reflection/SavedAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Utilities.Persistence;
-
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-public class SaveAttribute(string key = null) : Attribute
-{
-    public string Key { get; } = key;
-}
